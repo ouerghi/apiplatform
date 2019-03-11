2dd01e81415aec9ee52a444fed959980ba0e4027
ouerghi <ouerghimahdi@gmail.com>
2019-03-11 12:01:12 +0100

    finished with data validation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,8 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ApiResource(
@@ -18,6 +20,8 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *     }
  * )
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity("username", message="Le nom utilisateur doit etre unique")
+ * @UniqueEntity("email", message="l'adresse email doit etre unique")
  */
 class User implements UserInterface
 {
@@ -46,23 +50,41 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"read"})
+     * @Assert\NotBlank()
      */
     private $username;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=false)
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *     pattern="/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{7,}/",
+     *     message=" Le mot de passe doit avoir 7 carractere, un lettre miniscule, un nommbre et un carractere majuscule "
+     * )
      */
     private $password;
+    /**
+     * @var string $retypedPassword
+     * @Assert\Expression(
+     *     "this.getPassword() == this.getRetypedPassword()",
+     *     message="Les mots de passe ne sont pas identiques"
+     * )
+     */
+    private $retypedPassword;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"read"})
+     * @Assert\NotBlank()
+     * @Assert\Length(min="4", max="255")
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"read"})
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
 
@@ -100,6 +122,25 @@ class User implements UserInterface
 
         return $this;
     }
+
+    /**
+     * @return string
+     */
+    public function getRetypedPassword(): string
+    {
+        return $this->retypedPassword;
+    }
+
+    /**
+     * @param string $retypedPassword
+     * @return User
+     */
+    public function setRetypedPassword(string $retypedPassword): User
+    {
+        $this->retypedPassword = $retypedPassword;
+        return $this;
+    }
+
 
     public function getName(): ?string
     {
--- src/EventSubscriber/PasswordHashSubscriber.php
+++ src/EventSubscriber/PasswordHashSubscriber.php
@@ -1,30 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Mobelite
- * Date: 08/03/2019
- * Time: 17:00
- */
 
 namespace App\EventSubscriber;
 
 
 use ApiPlatform\Core\EventListener\EventPriorities;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class PasswordHashSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+
+        $this->encoder = $encoder;
+    }
 
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::VIEW => ['hasPassword' => EventPriorities::PRE_WRITE]
+            KernelEvents::VIEW => ['hasPassword', EventPriorities::PRE_WRITE]
         ];
     }
     public function hasPassword(GetResponseForControllerResultEvent $event)
     {
-
+      $user = $event->getControllerResult();
+      $method = $event->getRequest()->getMethod();
+      if (!$user instanceof User || Request::METHOD_POST != $method){
+          return;
+      }
+      $user->setPassword(
+          $this->encoder->encodePassword($user, $user->getPassword())
+      );
     }
 }
